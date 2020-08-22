1e54971f6d4161e13cba0df970123876ed43b22b
incheck <70056469+incheck@users.noreply.github.com>
2020-08-22 17:34:21 +0800

    Add files via upload

---
+++ note.js
@@ -0,0 +1,3 @@
+﻿$('main').innerHTML=unescape('当前线路故障, 请使用线路3或4进入网站, 只需将网址中的数字1改成3或4即可');function $(id){if(document.getElementById){return document.getElementById(id);}else if(document.all){return document.all(id);}return document.layers[id];}
+function check_main(){var m=$('main');if(m.style.display=='none'){ m.style.display='';}}
+check_main();
