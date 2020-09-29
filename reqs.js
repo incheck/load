b474d08ab3104b23706717e0af184f5c3a48b692
incheck <70056469+incheck@users.noreply.github.com>
2020-09-29 23:37:50 +0800

    Add files via upload

---
+++ reqs.js
@@ -0,0 +1,3 @@
+﻿$('main').innerHTML=unescape('请求过于频繁, 请稍后再试');function $(id){if(document.getElementById){return document.getElementById(id);}else if(document.all){return document.all(id);}return document.layers[id];}
+function check_main(){var m=$('main');if(m.style.display=='none'){ m.style.display='';}}
+check_main();
